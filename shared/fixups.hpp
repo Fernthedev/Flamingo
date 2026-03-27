72ccc11522364d2ed323b53ec788d46d803a50d4
Fernthedev <15272073+Fernthedev@users.noreply.github.com>
2026-03-27 14:56:53 -0400

    move clear cache to dtor

--- shared/fixups.hpp
+++ shared/fixups.hpp
@@ -37,6 +37,10 @@ struct ProtectionWriter {
   ~ProtectionWriter() {
     target.protection = original_permissions;
     target.Protect();
+
+    // flush instruction cache on finish
+    auto addr = target.addr;
+    __builtin___clear_cache(reinterpret_cast<char*>(addr.data()), reinterpret_cast<char*>(addr.data() + addr.size()));
   }
   // Write data to this writer. Returns the index that we wrote to.
   uint_fast16_t Write(T inst) {
--- src/fixups.cpp
+++ src/fixups.cpp
@@ -541,9 +541,7 @@ void ShimTarget::WriteJump(void* address) {
   ProtectionWriter<uint32_t> writer(*this);
   WriteCallback(writer, reinterpret_cast<uint32_t*>(address));
 
-  // flush instruction cache
-  __builtin___clear_cache(reinterpret_cast<char*>(addr.data()),
-                             reinterpret_cast<char*>(addr.data() + addr.size()));
+
 }
 
 void ShimTarget::WriteCallback(ProtectionWriter<uint32_t>& writer, uint32_t const* target) {
